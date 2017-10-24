6dd1120ffd6942a6d1f2be43ffab8aeb6c0d83b8
Robert <rl78746@yahoo.com>
2017-10-24 12:42:19 -0500

    split up application into modules

--- webdj/__init__.py
+++ webdj/__init__.py
@@ -1,1 +1,8 @@
-from .webdj import *
+# all the imports
+from flask import Flask
+
+app = Flask(__name__, instance_relative_config=True) # create the application instance :)
+app.config.from_pyfile('config.py')
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+import webdj.views
--- webdj/views.py
+++ webdj/views.py
@@ -1,47 +1,9 @@
-# all the imports
-import os
-import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, abort, \
+from webdj import app
+from webdj.model.security import pwd_context
+from flask import Flask, request, session, redirect, url_for, abort, \
      render_template, flash
-from .model.security import pwd_context
+from webdj.db_interface import get_db
 
-app = Flask(__name__, instance_relative_config=True) # create the application instance :)
-app.config.from_pyfile('config.py')
-
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
-
-def init_db():
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-
-@app.cli.command('initdb')
-def initdb_command():
-    """Initializes the database."""
-    init_db()
-    print('Initialized the database.')
-
-def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
-    
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-    
-@app.teardown_appcontext
-def close_db(error):
-    """Closes the database again at the end of the request."""
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
-        
 @app.route('/')
 def show_entries():
     db = get_db()
